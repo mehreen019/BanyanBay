a602ff6839e1f3fe79238d8d0e3eb2f6422783d5
mehreen019 <mehreenhossain@iut-dhaka.edu>
2025-01-21 17:37:25 +0600

    hero section done

--- banyan/src/App.tsx
+++ banyan/src/App.tsx
@@ -1,30 +1,12 @@
-import { ImageSlider } from "./ImageSlider"
-import { Mail } from 'lucide-react';
-import img1 from "./assets/img1.webp"
-import img2 from "./assets/img2.avif"
-import img3 from "./assets/img3.avif"
+import NavBar from "./components/NavBar"
+import Slider from "./components/Slider"
 
-const IMAGES = [
-  { url: img1, alt: "Car One" },
-  { url: img2, alt: "Car Two" },
-  { url: img3, alt: "Car Three" },
-]
 
 export default function App() {
   return (
-    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden">
-      <h1>BanyanBay</h1>
-      <div
-        style={{
-          //maxWidth: "1200px",
-          width: "100%",
-          //height: "50%",
-          aspectRatio: "10 / 3",
-          margin: "0 auto",
-        }}
-      >
-        <ImageSlider images={IMAGES} />
-      </div>
-    </main>  
+    <main>
+      <NavBar />
+      <Slider />
+    </main>
   )
 }
---
+++ banyan/src/components/Slider.tsx
@@ -0,0 +1,64 @@
+import img1 from "../assets/img1.jpg"
+import img2 from "../assets/img2.jpg"
+import img3 from "../assets/img3.jpg"
+import React, { useState, useEffect } from 'react'
+
+
+const IMAGES = [
+    { url: img1, alt: "Slider One", headertxt: "Banyan Bay", btntxt: "Learn More" },
+    { url: img2, alt: "Slider Two", headertxt: "Our Story", btntxt: "Learn More" },
+    { url: img3, alt: "Slider Three", headertxt: "Bbay Travels", btntxt: "Go to Website" },
+  ]
+
+const Slider = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentIndex((prev) => (prev >= IMAGES.length - 1 ? 0 : prev + 1));
+        }, 5000); // 5 seconds
+
+        return () => clearInterval(timer); // Cleanup on unmount
+    }, [currentIndex]); // Reset timer when currentIndex changes
+
+    const handleNext = () => {
+        setCurrentIndex((prev) => (prev >= IMAGES.length - 1 ? 0 : prev + 1));
+    };
+
+    const handlePrev = () => {
+        setCurrentIndex((prev) => (prev <= 0 ? IMAGES.length - 1 : prev - 1));
+    };
+    
+    return (
+        <div className='slider'>
+            <div className='list'>
+                {IMAGES.map((image, index) => (
+                    <div key={index} className={`item ${index === currentIndex ? 'active' : ''}`}>
+                        <img src={image.url} alt={image.alt} />
+                        <div className='content'>
+                            <p>design</p>
+                            <h2>{image.headertxt}</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+                            <button>{image.btntxt}</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <div className='arrows'>
+                <button className='prev' onClick={handlePrev} >{'<'}</button>
+                <button className='next' onClick={handleNext}>{'>'}</button>
+            </div>
+
+            <div className='scroll'>
+                <div>
+                    <p>Scroll Down</p>
+                    <p>To Discover More</p>
+                </div> 
+                <div className="mouse"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Slider
